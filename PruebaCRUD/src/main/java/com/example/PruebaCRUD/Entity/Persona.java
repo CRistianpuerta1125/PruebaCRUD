5379b6d8c26248bca6cd9bd3c2291e328182abb3
Damian Puerta <55324669+CRistianpuerta1125@users.noreply.github.com>
2023-12-14 19:23:04 -0500

    setters and getters

--- PruebaCRUD/src/main/java/com/example/PruebaCRUD/Entity/Persona.java
+++ PruebaCRUD/src/main/java/com/example/PruebaCRUD/Entity/Persona.java
@@ -23,4 +23,59 @@ public class Persona {
     @Column(name = "clave")
     private int clave;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getClave() {
+        return clave;
+    }
+
+    public void setClave(int clave) {
+        this.clave = clave;
+    }
 }
