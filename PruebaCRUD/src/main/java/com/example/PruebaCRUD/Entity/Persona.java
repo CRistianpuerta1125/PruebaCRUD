4c63509d4eaf77c0ecc9610977dd0310c2a40c0f
Damian Puerta <55324669+CRistianpuerta1125@users.noreply.github.com>
2023-12-14 19:21:29 -0500

    columnas para las propiedades

--- PruebaCRUD/src/main/java/com/example/PruebaCRUD/Entity/Persona.java
+++ PruebaCRUD/src/main/java/com/example/PruebaCRUD/Entity/Persona.java
@@ -1,22 +1,26 @@
 package com.example.PruebaCRUD.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name ="Humano")
 public class Persona {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+    @Column(name = "nombre")
     private String nombre;
+    @Column(name = "apellido")
     private String apellido;
+    @Column(name = "email")
     private String email;
+    @Column(name = "edad")
     private int edad;
+    @Column(name = "sexo")
     private String sexo;
+    @Column(name = "clave")
     private int clave;
 
 }
